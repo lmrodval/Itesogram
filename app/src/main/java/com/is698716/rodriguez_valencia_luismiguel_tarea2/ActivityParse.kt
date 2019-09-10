ef72e40469a9f8a97560b7a92169f3c3e30f50d4
Luis_Rod_Val <luis.rodriguez@bitcon.com.mx>
2019-09-10 02:48:15 -0500

    Commit with first design

---
+++ app/src/main/java/com/is698716/rodriguez_valencia_luismiguel_tarea2/ActivityParse.kt
@@ -0,0 +1,12 @@
+package com.is698716.rodriguez_valencia_luismiguel_tarea2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityParse : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parse)
+    }
+}
